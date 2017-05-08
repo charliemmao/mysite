d4b34503eddb366a0c31499bbdd2126d05c406dd
Charlie Mao <charliemao@hotmail.com>
2017-05-08 14:45:14 +0930

    Test githun
    First Commit

---
+++ PythonScript/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'chardet',
+    packages = ['chardet'],
+    version = '1.0.2',
+    description = 'Universal encoding detector',
+    author='Mark Pilgrim',
+)
+# python.exe setup.py bdist_wininst
+# Test git
